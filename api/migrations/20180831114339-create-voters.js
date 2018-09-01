80e94f808cd27f2fe0cf3fec3309e2bad4493e1d
Öner Çiller <onerciller@gmail.com>
2018-09-02 00:03:23 +0300

    fix format

--- api/migrations/20180831114339-create-voters.js
+++ api/migrations/20180831114339-create-voters.js
@@ -1,37 +1,37 @@
 module.exports = {
-	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable('voters', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
-			},
-			name: {
-				type: Sequelize.STRING,
-			},
-			role: {
-				type: Sequelize.ENUM('voter', 'master'),
-			},
-			status: {
-				type: Sequelize.ENUM('not_voted', 'voted'),
-				defaultvalue: 'not_voted',
-			},
-			created_at: {
-				allowNull: true,
-				type: Sequelize.DATE,
-			},
-			updated_at: {
-				allowNull: true,
-				type: Sequelize.DATE,
-			},
-			deleted_at: {
-				allowNull: true,
-				type: Sequelize.DATE,
-			},
-		});
-	},
-	down: queryInterface => {
-		return queryInterface.dropTable('voters');
-	},
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('voters', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.ENUM('voter', 'master')
+      },
+      status: {
+        type: Sequelize.ENUM('not_voted', 'voted'),
+        defaultValue: 'not_voted'
+      },
+      created_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.dropTable('voters');
+  }
 };
